5669a0791f8aa95658ad07e8b17db75f28abe968
anton <sada>
2022-09-21 18:38:12 +0300

    1

--- main.py
+++ main.py
@@ -2,13 +2,34 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 import time
+from pyzbar import pyzbar
+from glob import glob
+import cv2
+
+flag = True
+data_label = 'unpress'
+
+
+def decode_1(image):
+    # decodes all barcodes from an image
+    decoded_objects = pyzbar.decode(image)
+    for obj in decoded_objects:
+        # draw the barcode
+        print(f"Обнаружен штрих-код:\n{obj}")
+        # print barcode type & data
+        print("Тип:", obj.type)
+        print("Данные:", obj.data)
+        print()
+    return image
+
+
 Builder.load_string('''
 <CameraClick>:
     orientation: 'vertical'
     Camera:
         id: camera
         resolution: (640, 480)
-        play: False
+        play: True
     ToggleButton:
         text: 'Play'
         on_press: camera.play = not camera.play
@@ -18,7 +39,11 @@ Builder.load_string('''
         text: 'Capture'
         size_hint_y: None
         height: '48dp'
-        on_press: root.capture()
+        on_press: button()
+    Label:
+        font_size: "30sp"
+        text: root.data_label
+        
 ''')
 
 
@@ -33,9 +58,15 @@ class CameraClick(BoxLayout):
         camera.export_to_png("IMG_" + timestr)
         print("Captured")
 
+    def button(self):
+        if flag:
+            data_label = 'press'
+        else:
+            data_label = 'unpress'
+
+
 
 class TestCamera(App):
-
     def build(self):
         return CameraClick()
 
